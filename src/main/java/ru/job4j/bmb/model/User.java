889b7a62e8f0a2f37d5a659bb1d8a8184f001698
SNMiroshnichenko <miroshnichenkos@mail.ru>
2025-02-10 15:22:02 +0300

    @Profile | Пользователи системы [#505303]

---
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -0,0 +1,39 @@
+package ru.job4j.bmb.model;
+
+public class User {
+
+    private Long id;
+    private long clientId;
+    private long chatId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(long clientId) {
+        this.clientId = clientId;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    public User(Long id, long clientId, long chatId) {
+        this.id = id;
+        this.clientId = clientId;
+        this.chatId = chatId;
+    }
+
+}
